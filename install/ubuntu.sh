5bc931426f9abfcaf6f5adb20e3cc547534fbbc8
Gabriel Giro Resende <gabriel.resende@westwing.com.br>
2021-06-09 00:11:53 -0300

    Add ubuntu install

---
+++ install/ubuntu.sh
@@ -0,0 +1,37 @@
+mkdir ~/bin
+
+# Repositories
+sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+
+# Download and install the key from Google Linux Repository
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# Add key to the repository
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+sudo apt-get update
+
+# General Packages
+sudo apt-get -qq install curl
+sudo apt-get -qq install google-chrome-stable
+sudo apt-get -qq install jq
+sudo apt-get -qq install rcm
+sudo apt-get -qq install tmux
+sudo apt-get -qq install vim
+sudo apt-get -qq install zsh
+
+# Snap Apps
+sudo snap install slack --classic
+
+# Docker
+sudo apt-get install -qq linux-image-extra-$(uname -r) # Recommended extra packages, which
+sudo apt-get install -qq linux-image-extra-virtual     # allow to use `aufs` storage drivers.
+sudo apt-get install -qq apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable"
+sudo apt-get -qq install docker-ce
+
+# Docker Compose
+sudo apt -qq install docker-compose
