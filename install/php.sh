09400747a010143e232742291cc3734bfa2cfc58
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2021-06-11 23:28:47 -0300

    Add Composer install

--- install/php.sh
+++ install/php.sh
@@ -9,4 +9,25 @@ sudo add-apt-repository ppa:ondrej/php
 
 sudo apt update
 
-sudo apt-get -pp install php8.0
+sudo apt-get -qq install php8.0
+
+# Install last version of Composer (as shown here: https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md)
+COMPOSER_PATH=/usr/local/bin
+
+cd /tmp
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+sudo mv /tmp/composer.phar /usr/local/bin/composer
+rm composer-setup.php
+exit $RESULT
