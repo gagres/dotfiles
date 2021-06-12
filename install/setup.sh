8070ddb85c8e8547c251d722c96c7697e49808ea
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2021-06-11 22:00:50 -0300

    don't add file permissions to git

--- install/setup.sh
+++ install/setup.sh
@@ -13,6 +13,12 @@ if [ ! -d "$HOME/.nvm" ]; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 fi
 
+# Install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  wget -P /tmp https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+  sh /tmp/install.sh
+fi
+
 # Generate symlinks
 env RCRC=$HOME/.dotfiles/rcrc rcup
 rcup -v
